509e3e687d9ae370881a3213aac0d11890bc0723
satu sport <satusport.tv@gmail.com>
2026-05-25 09:35:30 +0700

    cart item

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,3 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mart/models/cart_item.dart';
+import 'package:flutter_mart/models/product.dart';
 
-class CartProvider extends ChangeNotifier {}
+class CartProvider extends ChangeNotifier {
+  List<CartItem> _cart = [];
+
+  List<CartItem> get cart => _cart;
+  void addItem(Product product) {
+    // terima product dari luar
+    final cartItem = CartItem(productType: product, quantity: 1);
+    // buat CartItem baru dari product itu
+    _cart.add(cartItem);
+    // masukkan ke _cart
+    notifyListeners();
+    // beritahu widget
+  }
+
+  void removeItem(Product product) {
+    // hapus CartItem yang productType-nya sama dengan product ini
+    _cart.removeWhere((item) => item.productType == product);
+    // beritahu widget
+    notifyListeners();
+  }
+}
